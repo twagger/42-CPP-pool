143dd5a18858cdb744709ec5d20ad184fad353e5
twagner <twagner@student.42.fr>
2022-02-15 10:14:27 +0100

    ex00 ok and init ex01

--- Module_02/ex01/Fixed.hpp
+++ Module_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 10:14:41 by twagner           #+#    #+#             */
-/*   Updated: 2022/02/15 09:58:19 by twagner          ###   ########.fr       */
+/*   Updated: 2022/02/15 10:13:13 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ class Fixed
 	public:
 		// Constructors and destructor
 		Fixed(void);
+		Fixed(int value);
+		Fixed(float value);
 		Fixed(Fixed const &src);
 		~Fixed(void);
 
@@ -28,10 +30,14 @@ class Fixed
 		// member functions
 		int		getRawBits(void) const;
 		void	setRawBits(int const raw);
+		float	toFloat(void) const;
+		int		toInt(void)	const;
 
 	private:
 		int					_value;
 		static int const	_nbBits;
 };
+
+std::ostream	&operator<<(std::ostream &o, Fixed const &i);
 
 #endif
