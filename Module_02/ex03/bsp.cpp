8012479a2375812ed59a9e782e376422ee7e87f9
twagner <twagner@student.42.fr>
2022-02-18 16:22:09 +0100

    more detail on the sign function

--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -6,13 +6,19 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 11:02:15 by twagner           #+#    #+#             */
-/*   Updated: 2022/02/18 16:17:58 by twagner          ###   ########.fr       */
+/*   Updated: 2022/02/18 16:21:36 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 #include <iostream>
+
+
+/*
+** This function check on which plan (separated by p1 -> p2 line) is the point
+** Returns >0 for one side, <0 for the other side, 0 if the point is on the line
+*/
 
 static float	sign (Point point, Point p1, Point p2)
 {
@@ -22,7 +28,7 @@ static float	sign (Point point, Point p1, Point p2)
 
 /*
 ** The BSP function will check for each "side" of the triangle if it is on the left,
-** right or on the side with the "sign" function (returning >0, <0 or 0)
+** right or on the line with the "sign" function (returning >0, <0 or 0)
 ** If the point is on the same "side" for each part of the triangle, it is inside.
 */
 
