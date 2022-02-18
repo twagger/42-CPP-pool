dcfb2e4892f2b8d7ca5e120b23bbc732e29a0bc1
twagner <twagner@student.42.fr>
2022-02-18 10:04:30 +0100

    fixed alignments

--- Module_02/ex01/main.cpp
+++ Module_02/ex01/main.cpp
@@ -6,21 +6,21 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 10:12:03 by twagner           #+#    #+#             */
-/*   Updated: 2022/02/15 10:51:04 by twagner          ###   ########.fr       */
+/*   Updated: 2022/02/18 10:03:05 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include "Fixed.hpp"
 
-int main( void )
+int main(void)
 {
 	Fixed		a;
-	Fixed const	b( 10 );
-	Fixed const	c( 42.42f );
-	Fixed const	d( b );
+	Fixed const	b(10);
+	Fixed const	c(42.42f);
+	Fixed const	d(b);
 	
-	a = Fixed( 1234.4321f );
+	a = Fixed(1234.4321f);
 	
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
