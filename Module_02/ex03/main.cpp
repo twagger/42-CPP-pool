2090b4c92d0ea25a67c040210724a996722c5611
twagner <twagner@student.42.fr>
2022-02-18 16:34:22 +0100

    added another test with copy constructor and default one on Point

--- Module_02/ex03/main.cpp
+++ Module_02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 10:12:03 by twagner           #+#    #+#             */
-/*   Updated: 2022/02/18 16:06:28 by twagner          ###   ########.fr       */
+/*   Updated: 2022/02/18 16:32:19 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ int main(void)
 	std::cout << "-----------------------" << std::endl;
 	std::cout << "|        TEST 2       |" << std::endl;
 	std::cout << "-----------------------" << std::endl;
-
 	Point p4(-3.2, 0);
 	Point p5(3.22, 2.48);
 	Point p6(3.5, -2.81);
@@ -39,5 +38,15 @@ int main(void)
 	std::cout << "Point in  : " << bsp(p4, p5, p6, point_in_2) << std::endl;
 	std::cout << "Point out : " << bsp(p4, p5, p6, point_out_2) << std::endl;
 
+	std::cout << "-----------------------" << std::endl;
+	std::cout << "|        TEST 3       |" << std::endl;
+	std::cout << "-----------------------" << std::endl;
+	Point p7(-3.2, 0);
+	Point p8(p7);
+	Point p9;
+	Point point_out_3(-10, 0.1);
+	Point point_out_4(-4, 3.2);
+	std::cout << "Point out : " << bsp(p7, p8, p9, point_out_3) << std::endl;
+	std::cout << "Point out : " << bsp(p7, p8, p9, point_out_4) << std::endl;
 	return (0);
 }
