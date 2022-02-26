87aab7be0c3019ffba412611c043fe725fba92d4
twagner <twagner@student.42.fr>
2022-02-26 10:04:51 +0100

    removing debugging info

--- Module_02/ex03/bsp.cpp
+++ Module_02/ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/18 11:02:15 by twagner           #+#    #+#             */
-/*   Updated: 2022/02/26 09:48:23 by twagner          ###   ########.fr       */
+/*   Updated: 2022/02/26 10:04:22 by twagner          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,7 +43,6 @@ bool	bsp(Point const a, Point const b, Point const c, Point const point)
 	d1 = sign(point, a, b);
 	d2 = sign(point, b, c);
 	d3 = sign(point, c, a);
-	std::cout << d1 << " " << d2 << " " << d3 << std::endl;
 	has_neg = (d1 < 0) || (d2 < 0) || (d3 < 0);
 	has_pos = (d1 > 0) || (d2 > 0) || (d3 > 0);
 
