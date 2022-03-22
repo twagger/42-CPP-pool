b43aaa6ad8d7d9a1820d8f62a5a74612cb2338bd
twagner <twagner@student.42.fr>
2022-03-22 16:24:46 +0100

    module 08 init

---
+++ Module_08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: twagner <twagner@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 15:53:37 by twagner           #+#    #+#             */
+/*   Updated: 2022/03/22 16:04:06 by twagner          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template< typename T >
+void	easyfind(T container, int to_find)
+{
+	
+};
+
+#endif;
